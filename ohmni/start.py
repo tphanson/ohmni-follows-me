b21ed873c73fe4bc1c990d419edc8d7384647ee6
tphanson <tphanson@kambria.io>
2020-01-07 15:56:27 +0700

    debug

--- ohmni/start.py
+++ ohmni/start.py
@@ -66,11 +66,11 @@ def start(server, botshell):
                     (obj.bbox.ymax-obj.bbox.ymin)
                 print(xmed)
                 if xmed < 310:
-                    # Turn right
-                    botshell.sendall("manual_move 50 50\n".encode())
-                elif xmed > 330:
                     # Turn left
                     botshell.sendall("manual_move -50 -50\n".encode())
+                elif xmed > 330:
+                    # Turn right
+                    botshell.sendall("manual_move 50 50\n".encode())
                 else:
                     # Stop
                     botshell.sendall("manual_move 0 0\n".encode())
