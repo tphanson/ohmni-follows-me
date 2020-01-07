d4650c4bcaaae10a2787d4a0cd54460647d63c25
tphanson <tphanson@kambria.io>
2020-01-07 14:32:02 +0700

    debug

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import sys
 
 import tensorflow as tf
-from tests import came, wheel, cam
+from tests import wheel, cam
 from ohmni import start as ohmni
 
 tf.get_logger().setLevel('ERROR')
 
 if __name__ == "__main__":
     if sys.argv[1] == "--test":
-        if sys.argv[2] == 'came':
-            came.start()
         if sys.argv[2] == 'wheel':
             wheel.start()
         if sys.argv[2] == 'cam':
--- tests/cam.py
+++ tests/cam.py
@@ -1,4 +1,3 @@
-import argparse
 import platform
 import subprocess
 from PIL import Image
@@ -44,15 +43,8 @@ def start():
     while True:
 
         datagram = server.recv(65536)
-        print(1)
         if not datagram:
-            print(2)
             break
-        print(3)
-        # Dump contents for view here
-        #print("-" * 20)
-        # print(datagram)
-        # print(len(datagram))
 
         # Handle based on state machine
         if state == SockState.SEARCHING:
@@ -69,7 +61,6 @@ def start():
             msgtype = unpack("I", datagram[8:12])
             if msgtype[0] == 1:
                 params = unpack("IIII", datagram[12:28])
-                #print("Got frame start msg:", params)
 
                 state = SockState.FILLING
                 imgdata = bytearray()
@@ -78,14 +69,6 @@ def start():
                 frameheight = params[1]
                 frameformat = params[2]
                 framesize = params[3]
-
-            # elif msgtype[0] == 2:
-                # END FRAME - for now no-op
-                #print("Got end frame.")
-
-            # else:
-                # No op for other
-                #print("Got other msgtype.")
 
         # Filling image buffer now
         elif state == SockState.FILLING:
@@ -105,11 +88,8 @@ def start():
 
             print(rgbim)
 
-            # ADD YOUR LOGIC HERE TO PROCESS newim/rgbim
-
             # Go back to initial state
             state = SockState.SEARCHING
-            #print("Got complete frame")
 
     print("-" * 20)
     print("Shutting down...")
