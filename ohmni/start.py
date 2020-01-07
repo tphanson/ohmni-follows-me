47685b7b29ca73fb452a9c9a470c676373529fcc
tphanson <tphanson@kambria.io>
2020-01-07 16:22:05 +0700

    debug

--- ohmni/start.py
+++ ohmni/start.py
@@ -4,7 +4,7 @@ from utils import image, camera
 from ohmni.humandetection import HumanDetection
 from ohmni.tracker import IdentityTracking
 
-VELOCITY = 100
+VELOCITY = 200
 
 def start(server, botshell):
     idtr = IdentityTracking()
