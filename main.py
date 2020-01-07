3027e7f8d94938952c3f531c54c853c3a28a6557
tphanson <tphanson@kambria.io>
2020-01-07 11:32:49 +0700

    test camera

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
 
 import tensorflow as tf
+from test import camera
 from ohmni import start as ohmni
 
 tf.get_logger().setLevel('ERROR')
 
 if __name__ == "__main__":
     if sys.argv[1] == "--test":
-        pass
+        if sys.argv[2] == 'camera':
+            camera.start()
 
     elif sys.argv[1] == '--ohmni':
         if sys.argv[2] == 'start':
---
+++ test/__init__.py
@@ -0,0 +1,125 @@
+import argparse
+import platform
+import subprocess
+import signal
+from PIL import Image
+from PIL import ImageDraw
+
+import socket
+import os
+import os.path
+import time
+from enum import Enum
+from struct import *
+
+# Open connection to bot shell
+botshell = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+botshell.connect("/app/bot_shell.sock")
+#botshell.sendall(b"say hello\n")
+botshell.sendall(b"wake_head\n")
+
+if os.path.exists("/dev/libcamera_stream"):
+    os.remove("/dev/libcamera_stream")\
+
+print("Opening socket...")
+server = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+server.bind("/dev/libcamera_stream")
+os.chown("/dev/libcamera_stream", 1047, 1047)
+
+
+class SockState(Enum):
+    SEARCHING = 1
+    FILLING = 2
+
+
+def keyboardInterruptHandler(signal, frame):
+    print("Stopping...")
+    botshell.sendall("manual_move 0 0\n".encode())
+    exit(0)
+
+
+signal.signal(signal.SIGINT, keyboardInterruptHandler)
+
+
+def main():
+    state = SockState.SEARCHING
+    imgdata = None
+    framewidth = 0
+    frameheight = 0
+    frameformat = 0
+    framesize = 0
+
+    lastrot = 0
+    lastvel = 0
+
+    print("Listening...")
+    server.settimeout(0.5)
+    while True:
+
+        try:
+            datagram = server.recv(65536)
+        except socket.timeout:
+            if lastrot != 0 or lastvel != 0:
+                print("socket timeout, clear manual_move")
+                botshell.sendall("manual_move 0 0\n".encode())
+                lastrot = 0
+                lastvel = 0
+            continue
+
+        if not datagram:
+            break
+
+        # Handle based on state machine
+        if state == SockState.SEARCHING:
+
+            # Check for non-control packets
+            if len(datagram) < 12 or len(datagram) > 64:
+                continue
+
+            # Check for magic
+            if not datagram.startswith(b'OHMNICAM'):
+                continue
+
+            # Unpack the bytes here now for the message type
+            msgtype = unpack("I", datagram[8:12])
+            if msgtype[0] == 1:
+                params = unpack("IIII", datagram[12:28])
+
+                state = SockState.FILLING
+                imgdata = bytearray()
+
+                framewidth = params[0]
+                frameheight = params[1]
+                frameformat = params[2]
+                framesize = params[3]
+
+        # Filling image buffer now
+        elif state == SockState.FILLING:
+
+            # Append to buffer here
+            imgdata.extend(datagram)
+
+            # Check size
+            if len(imgdata) < framesize:
+                continue
+
+            # Resize and submit
+            imgbytes = bytes(imgdata)
+            newim = Image.frombytes(
+                "L", (framewidth, frameheight), imgbytes, "raw", "L")
+            rgbim = newim.convert("RGB")
+
+            # Go back to initial state
+            state = SockState.SEARCHING
+            #print("Got complete frame")
+
+    print("-" * 20)
+    print("Shutting down...")
+    server.close()
+
+    os.remove("/dev/libcamera_stream")
+    print("Done")
+
+
+if __name__ == '__main__':
+    main()
