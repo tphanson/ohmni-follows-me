147c789eefbfa5b1b51dfbe1b90c05a4a5d736de
tphanson <tphanson@kambria.io>
2020-01-07 14:28:55 +0700

    debug

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import sys
 
 import tensorflow as tf
-from tests import camera, wheel, cam
+from tests import came, wheel, cam
 from ohmni import start as ohmni
 
 tf.get_logger().setLevel('ERROR')
 
 if __name__ == "__main__":
     if sys.argv[1] == "--test":
-        if sys.argv[2] == 'camera':
-            camera.start()
+        if sys.argv[2] == 'came':
+            came.start()
         if sys.argv[2] == 'wheel':
             wheel.start()
         if sys.argv[2] == 'cam':
