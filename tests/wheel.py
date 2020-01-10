f358cf12bf523db385ec4621905bed9119a59c59
tphanson <tphanson@kambria.io>
2020-01-10 11:13:40 +0700

    benchmark

--- tests/wheel.py
+++ tests/wheel.py
@@ -2,8 +2,11 @@ import time
 
 
 def start(botshell):
-    botshell.sendall("manual_move 750 -750\n".encode())
+    botshell.sendall("manual_move 500 -500\n".encode())
     time.sleep(5)
     botshell.sendall("manual_move 0 0\n".encode())
-    # botshell.sendall("manual_move 100 -100\n".encode())
-    # time.sleep(2)
+    time.sleep(2)
+    botshell.sendall("manual_move -100 100\n".encode())
+    time.sleep(5)
+    botshell.sendall("manual_move 0 0\n".encode())
+    time.sleep(2)
