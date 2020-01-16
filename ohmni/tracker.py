ab532faa14642fa5cfb7fe2a5b188142ce846a4f
tphanson <tphanson@kambria.io>
2020-01-16 12:52:10 +0700

    update flow

--- ohmni/tracker.py
+++ ohmni/tracker.py
@@ -134,6 +134,7 @@ class IdentityTracking:
         y = np.array([])
         for i in x:
             y = np.append(y, self.mymodel(tf.convert_to_tensor([i])))
+        # y = self.mymodel(x)
         clend2 = time.time()
         print('\t Model {:.4f}'.format(clend2-clend1))
         predictions = tf.reshape(y, [-1])
