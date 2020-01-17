240abd186286696f05aba1c38ea393577adb08b4
tphanson <tphanson@kambria.io>
2020-01-17 16:01:34 +0700

    test performance

--- ohmni/humandetection.py
+++ ohmni/humandetection.py
@@ -1,7 +1,7 @@
 import os
 import tflite_runtime.interpreter as tflite
 
-from utils import detect
+from utils.bbox import Object, BBox
 
 EDGETPU_SHARED_LIB = 'libedgetpu.so.1'
 LABELS = os.path.join(os.path.dirname(
@@ -11,10 +11,12 @@ MODELS = os.path.join(os.path.dirname(
 
 
 class HumanDetection:
-    def __init__(self, confidence=0.6):
+    def __init__(self, confidence=0.5):
         self.labels = self.load_labels()
         self.interpreter = self.make_interpreter(MODELS)
         self.confidence = confidence
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
 
     def load_labels(self):
         with open(LABELS, 'r', encoding='utf-8') as labels_file:
@@ -34,7 +36,27 @@ class HumanDetection:
 
     def predict(self, img):
         self.interpreter.allocate_tensors()
-        detect.set_input(self.interpreter, img)
+        self.interpreter.set_tensor(self.input_details[0]['index'], [img])
         self.interpreter.invoke()
-        objs = detect.get_output(self.interpreter, self.confidence, True)
-        return objs
+        boxes = self.interpreter.get_tensor(self.output_details[0]['index'])[0]
+        class_ids = self.interpreter.get_tensor(
+            self.output_details[1]['index'])[0]
+        scores = self.interpreter.get_tensor(
+            self.output_details[2]['index'])[0]
+        count = int(self.interpreter.get_tensor(
+            self.output_details[3]['index'])[0])
+
+        def make(i):
+            ymin, xmin, ymax, xmax = boxes[i]
+            return Object(
+                id=0,
+                frame=0,
+                label=int(class_ids[i]),
+                score=scores[i],
+                bbox=BBox(xmin=int(xmin*300),
+                          ymin=int(ymin*300),
+                          xmax=int(xmax*300),
+                          ymax=int(ymax*300)))
+
+        return [make(i) for i in range(count) if (scores[i] >= self.confidence and int(class_ids[i]) == 0)]
+
--- ohmni/tracker.py
+++ ohmni/tracker.py
@@ -116,8 +116,7 @@ class IdentityTracking:
 
     def predict(self, bboxes_batch, obj_imgs_batch):
         movstart = time.time()
-        mov_features = self.mextractor(
-            tf.convert_to_tensor(bboxes_batch))
+        mov_features = self.mextractor(tf.convert_to_tensor(bboxes_batch))
         movend = time.time()
         print('MOV estimated time {:.4f}'.format(movend-movstart))
 
