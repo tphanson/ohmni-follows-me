98ad3f9a7b60e8541fc9a9e7b3af9e38db09dd12
tphanson <tphanson@kambria.io>
2020-02-11 14:47:50 +0700

    dynamic test

--- tests/wheel.py
+++ tests/wheel.py
@@ -3,7 +3,7 @@ import time
 
 def start(botshell):
     botshell.sendall("manual_move 2600 -2400\n".encode())
-    time.sleep(1)
+    time.sleep(3)
     botshell.sendall("manual_move 500 -500\n".encode())
     time.sleep(5)
     botshell.sendall("manual_move 0 0\n".encode())
