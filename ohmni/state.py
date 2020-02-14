f859276248fe7845b91d2e6842d4117d1f436018
tphanson <tphanson@kambria.io>
2020-02-14 14:53:41 +0700

    test pose

--- ohmni/state.py
+++ ohmni/state.py
@@ -1,14 +1,19 @@
+DEFAUT_DELAY = 1200
+
+
 class StateMachine:
     def __init__(self):
         self.states = ['idle', 'run']
         self.current_index = 0
         self.current_state = self.states[self.current_index]
-        self.state_counter = 1200
+        self.state_counter = DEFAUT_DELAY
 
     def delay(self):
         if self.state_counter <= 0:
+            self.state_counter = DEFAUT_DELAY
             return True
         if self.current_state != 'run':
+            self.state_counter = DEFAUT_DELAY
             return True
         self.state_counter -= 1
         return False
