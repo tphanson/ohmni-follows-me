79e8335e7d8917db7bf2524467dff90bb2f54f00
tphanson <tphanson@kambria.io>
2020-02-17 13:24:23 +0700

    test controllers

--- utils/camera.py
+++ utils/camera.py
@@ -52,5 +52,6 @@ def fetch(server):
             img_arr = np.fromstring(imgbytes, np.uint8)
             bwim = np.reshape(img_arr, (framewidth, frameheight))
             bgrim = cv.cvtColor(bwim, cv.COLOR_GRAY2RGB)
+            print(bgrim)
             state = SockState.SEARCHING
             return bgrim
