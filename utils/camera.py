f57204fdfe9b2ff3dc4325fd2c2a846d37f88f23
tphanson <tphanson@kambria.io>
2020-02-17 10:39:46 +0700

    test controllers

--- utils/camera.py
+++ utils/camera.py
@@ -50,6 +50,7 @@ def fetch(server):
             #     "L", (framewidth, frameheight), imgbytes, "raw", "L")
             # rgbim = newim.convert("RGB")
             nparr = np.fromstring(imgbytes, np.uint8)
+            print(nparr)
             rgbim = cv.imdecode(nparr, cv.IMREAD_COLOR)
             print(rgbim)
             state = SockState.SEARCHING
