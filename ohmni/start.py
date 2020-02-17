5ea9b09a9f07dbc3c583d1bd125f0d987bba3c16
tphanson <tphanson@kambria.io>
2020-02-17 13:55:02 +0700

    test controllers

--- ohmni/start.py
+++ ohmni/start.py
@@ -92,11 +92,11 @@ def start(server, botshell):
         timer = cv.getTickCount()
         imgstart = time.time()
         bgrimg, rbgimg = camera.fetch(server)
-        np.savetxt('bgrimg.txt', bgrimg)
+        np.savetxt('bgrimg.txt', str(bgrimg))
         if rbgimg is None:
             continue
         img = image.convert_pil_to_cv(rbgimg)
-        np.savetxt('rbgimg.txt', img)
+        np.savetxt('rbgimg.txt', str(img))
         break
         imgend = time.time()
         print('Image estimated time {:.4f}'.format(imgend-imgstart))
