8d63aa6630d4f94cca4a6d036f4fe820672b9a57
tphanson <tphanson@kambria.io>
2020-02-18 14:04:14 +0700

    add supportive states

--- ohmni/state.py
+++ ohmni/state.py
@@ -17,17 +17,14 @@ class NoiseReduction:
 
     def input(self, bit, seconds):
         if self.start is None:
-            print("Start ==================================")
             self.start = self.__get_timestamp()
         if self.__get_timestamp()-self.start < seconds:
-            print("Bit", bit)
             self.register = np.append(self.register, bit)
             return None
         else:
             mean = np.mean(self.register)
             self.__reset()
-            print("End ====================================", mean)
-            return mean >= self.threshold
+            return bool(mean >= self.threshold)
 
 
 class StateMachine:
@@ -39,18 +36,14 @@ class StateMachine:
 
     def __next(self):
         self.current_index = (self.current_index+1) % len(self.states)
-        print("Next 1",  self.current_index)
         self.current_state = self.states[self.current_index]
-        print("Next 2",  self.current_state)
 
     def __back(self):
         self.current_index = (self.current_index-1) % len(self.states)
         self.current_state = self.states[self.current_index]
 
     def __change_state(self, denoise_status):
-        print('__change_state', type(denoise_status), denoise_status is True)
         if denoise_status:
-            print("Vo ne ba")
             self.__next()
         else:  # No change
             pass
@@ -60,19 +53,15 @@ class StateMachine:
 
     def set_state(self, next_flag):
         if self.current_state == 'init_idle':
-            print(1)
             self.__change_state(True)
         elif self.current_state == 'idle':
             ok = self.denoise.input(int(next_flag), 1)
             self.__change_state(ok)
-            print(2, ok, self.current_state)
         elif self.current_state == 'init_run':
-            print(3)
             self.__change_state(True)
         elif self.current_state == 'run':
             ok = self.denoise.input(int(next_flag), 20)
             self.__change_state(ok)
-            print(4, ok, self.current_state)
         else:
             self.__throw_error()
 
