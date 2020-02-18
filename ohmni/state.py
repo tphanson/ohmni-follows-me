3dca1d0517cddea2190bf7830ac0341d27c68825
tphanson <tphanson@kambria.io>
2020-02-18 12:03:44 +0700

    add supportive states

--- ohmni/state.py
+++ ohmni/state.py
@@ -15,7 +15,8 @@ class NoiseReduction:
         if self.start is None:
             self.start = self.get_timestamp()
         if self.get_timestamp()-self.start < seconds:
-            print("Time", self.get_timestamp()-self.start)
+            print("Time 1", self.start)
+            print("Time 2", self.get_timestamp())
             self.register = np.append(self.register, bit)
             return None
         else:
