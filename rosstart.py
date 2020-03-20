08467a286f15ff1311cc3f6b04fc210eba2ca894
tphanson <tphanson@kambria.io>
2020-03-20 14:09:59 +0700

    test ros

--- rosstart.py
+++ rosstart.py
@@ -1,12 +1,12 @@
-
 #!/usr/bin/env python
 
 import socket
 from ohmni import start as ohmni
 
 
-# Init botshell
-botshell = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-botshell.connect("/app/bot_shell.sock")
-botshell.sendall(b"wake_head\n")
-ohmni.start(botshell)
+if __name__ == "__main__":
+  # Init botshell
+  botshell = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+  botshell.connect("/app/bot_shell.sock")
+  botshell.sendall(b"wake_head\n")
+  ohmni.start(botshell)
