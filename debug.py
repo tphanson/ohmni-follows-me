903dc84d779f94ae8bc369d59b63be5cb3463aa6
tphanson <tphanson@kambria.io>
2020-03-27 10:40:00 +0700

    debug

--- debug.py
+++ debug.py
@@ -6,12 +6,13 @@ import numpy as np
 from datetime import datetime
 
 
-def stringToRGB(base64_string):
+def compressed_to_cv(msg):
     start = time.time()
-    imgdata = base64.b64decode(base64_string)
-    img = np.fromstring(imgdata, dtype=np.uint8)
-    img = img.reshape((480, 640, 3))
-    img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+    data = base64.b64decode(msg['data'])
+    img = np.fromstring(data, dtype=np.uint8)
+    print(img.shape)
+    # img = img.reshape((480, 640, 3))
+    # img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
     end = time.time()
     print('Image estimated time {:.4f}'.format(end-start))
     return img
@@ -26,7 +27,7 @@ def callback(msg):
     img_time = float(str(stamp['secs'])+'.'+str(stamp['nsecs']))
     print("Image time", datetime.fromtimestamp(img_time))
     print("Current time", datetime.now())
-    # img = stringToRGB(msg['data'])
+    img = compressed_to_cv(msg)
     # print(img.shape)
     end = time.time()
     print('Callback estimated time {:.4f}'.format(end-start))
