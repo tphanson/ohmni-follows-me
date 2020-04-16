ebd198dc4383c776595263bd069a79aff920af90
tphanson <tphanson@kambria.io>
2020-04-16 11:49:10 +0700

    bugfix

--- detection/coco.py
+++ detection/coco.py
@@ -4,9 +4,9 @@ import cv2 as cv
 
 EDGETPU_SHARED_LIB = 'libedgetpu.so.1'
 LABELS = os.path.join(os.path.dirname(
-    os.path.abspath(__file__)), "../models/tpu/coco_labels.txt")
+    os.path.abspath(__file__)), "../tpu/coco_labels.txt")
 MODELS = os.path.join(os.path.dirname(
-    os.path.abspath(__file__)), "../models/tpu/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite")
+    os.path.abspath(__file__)), "../tpu/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite")
 
 
 class HumanDetection:
