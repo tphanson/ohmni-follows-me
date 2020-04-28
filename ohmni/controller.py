ede75a10c24304f55326983b226a2ac223cd995e
tphanson <tphanson@kambria.io>
2020-04-28 09:57:22 +0700

    debug

--- ohmni/controller.py
+++ ohmni/controller.py
@@ -175,8 +175,8 @@ class Autonomy:
         self.botshell.sendall(f'neck_angle {pos}\n'.encode())
 
     def wait(self):
-        print('*** Autonomy move:', 1.01, 0)
-        self.botshell.sendall(b'follow_me 1.01 0\n')
+        print('*** Autonomy move:', 0, 0)
+        self.botshell.sendall(b'follow_me 0 0\n')
 
     def rest(self):
         print('*** Autonomy move:', 1.01, 0)
