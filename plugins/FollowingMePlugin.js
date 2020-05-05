39b7d884de2c09780c6d39ce59f8fbbc4d0a7911
tphanson <tphanson@kambria.io>
2020-05-05 14:22:56 +0700

    debug

---
+++ plugins/FollowingMePlugin.js
@@ -0,0 +1,35 @@
+var { spawn } = require('child_process');
+var LogCatter = require('logcatter');
+var Log = new LogCatter('SampleOhmniJSPlugin');
+
+class FollowingMePlugin {
+
+  constructor(botnode) {
+    // States
+    this.botnode = botnode;
+    this.python3 = null;
+    // Log loading msg
+    Log.i("  -> FollowingMePlugin loaded!");
+    // Bind some extra bot shell commands
+    this.start_cmd();
+    this.stop_cmd();
+  }
+
+  start_cmd() {
+    this.botnode._botshell.cmd_start_following_me = function (params, rl) {
+      this.log(rl, 'Start following me.');
+      this.python3 = spawn('python3', ['/home/ohmnidev/ohmni-follows-me/main.py', '--ohmni', 'start']);
+    }
+  }
+
+  stop_cmd() {
+    this.botnode._botshell.cmd_stop_following_me = function (params, rl) {
+      if (!this.python3) return this.log(rl, 'Following me process have not started yet.');
+      this.log(rl, 'Stop following me.');
+      this.python3.kill();
+    }
+  }
+
+}
+
+module.exports = FollowingMePlugin;
