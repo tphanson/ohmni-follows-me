ec001875fad5e32f78cf7c7636011a941c727283
tphanson <tphanson@kambria.io>
2020-05-07 15:04:26 +0700

    fix docker

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Pull docker image
 docker rm ofm
-docker run -dt --network="host" -v /data/data/com.ohmnilabs.telebot_rtc/files:/app -v /data:/data -w /home/ohmnidev/ohmni-follows-me --privileged -v /dev:/dev --security-opt seccomp:unconfined --name ofm tphanson/ohmni_follows_me:0.0.3
+docker run -d --network="host" -v /data/data/com.ohmnilabs.telebot_rtc/files:/app -v /data:/data -w /home/ohmnidev/ohmni-follows-me --privileged -v /dev:/dev --security-opt seccomp:unconfined --name ofm tphanson/ohmni_follows_me:0.0.3
 docker stop ofm
 # Install plugins
 docker cp ofm:/home/ohmnidev/ohmni-follows-me/plugins/followingMePlugin.js /data/data/com.ohmnilabs.telebot_rtc/files/plugins/
