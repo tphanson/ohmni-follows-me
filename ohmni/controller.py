0714a2b3cd4fe06a46422f2048bebe20906456ea
tphanson <tphanson@kambria.io>
2020-05-07 16:27:40 +0700

    debug

--- ohmni/controller.py
+++ ohmni/controller.py
@@ -178,8 +178,8 @@ class Autonomy:
 
     def wait(self):
         t = int(time.time()*1000)
-        print('*** Autonomy move:', 1, 0, t)
-        self.botshell.sendall(f'follow_me 1 0 {t}\n'.encode())
+        print('*** Autonomy move:', 0.5, 0, t)
+        self.botshell.sendall(f'follow_me 0.5 0 {t}\n'.encode())
 
     def rest(self):
         t = int(time.time()*1000)
