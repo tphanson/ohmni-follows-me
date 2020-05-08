68b92ede54604e21240c7d48b1a3b1e1e9c4311d
tphanson <tphanson@kambria.io>
2020-05-08 14:59:46 +0700

    debug

--- tests/wheel.py
+++ tests/wheel.py
@@ -2,15 +2,15 @@ import time
 
 
 def start(botshell):
-    botshell.sendall(b'start_autonomy\n')
-    time.sleep(5)
-    print('Start')
-    botshell.sendall(b'follow_me 1 0\n')
-    time.sleep(5)
-    botshell.sendall(b'stop_autonomy\n')
-
-    # botshell.sendall("manual_move 500 -500\n".encode())
+    # botshell.sendall(b'start_autonomy\n')
     # time.sleep(5)
+    # print('Start')
+    # botshell.sendall(b'follow_me 1 0\n')
+    # time.sleep(5)
+    # botshell.sendall(b'stop_autonomy\n')
+
+    botshell.sendall("manual_move 0 -1800\n".encode())
+    time.sleep(5)
     # botshell.sendall("manual_move 0 0\n".encode())
     # time.sleep(2)
     # botshell.sendall("manual_move -500 500\n".encode())
