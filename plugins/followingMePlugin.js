19951c9c36a39d2aa44df48db645e339b7788f1e
tphanson <tphanson@kambria.io>
2020-05-26 11:32:46 +0700

    update plugin

--- plugins/followingMePlugin.js
+++ plugins/followingMePlugin.js
@@ -1,29 +1,73 @@
-var { exec } = require('child_process');
+var { execSync } = require('child_process');
 var BotShell = require('/data/data/com.ohmnilabs.telebot_rtc/files/assets/node-files/bot_shell');
+const { AutonomyController } = require('/data/data/com.ohmnilabs.telebot_rtc/files/assets/node-files/autonomy_controller');
+
+const STACK = {
+  AUTONOMY: 0,
+  NORMAL: 1
+};
+
 
 class FollowingMePlugin {
-  constructor() {
+  constructor(botnode) {
+    var self = this;
     // States
     var tag = 'FolowingMePlugin';
     // Log loading msg
     console.log(tag, 'loaded!');
-    // Bind some extra bot shell commands
+
+    AutonomyController.prototype.cmd_startFollowingMe = function (mode) {
+      if (mode == 'heteronomy') {
+        console.log(this.TAG, 'Start following me in HETERONOMY mode.');
+        if (botnode.currentStack === STACK.AUTONOMY) botnode.switchToNormalStack();
+        execSync('docker start ofm && docker exec -d ofm python3 main.py --ohmni heteronomy');
+        return self.notify('start_following_me', 'success', 'heteronomy');
+      }
+      if (mode == 'autonomy') {
+        console.log(this.TAG, 'Start following me in AUTONOMY mode.');
+        if (botnode.currentStack === STACK.NORMAL) botnode.switchAutonomyStack();
+        execSync('docker start ofm && docker exec -d ofm python3 main.py --ohmni autonomy');
+        return self.notify('start_following_me', 'success', 'autonomy');
+      }
+    }
+    AutonomyController.prototype.cmd_stopFollowingMe = function () {
+      console.log(this.TAG, 'Stop following me.');
+      execSync('docker stop ofm');
+      return self.notify('stop_following_me', 'success');
+    }
+
+    // Auto open camera before start following me
     BotShell.prototype.cmd_start_following_me = function (params) {
-      if (params[0] == 'heteronomy') {
-        console.log(tag, 'Start following me in HETERONOMY mode.');
-        exec('docker start ofm && docker exec -d ofm python3 main.py --ohmni heteronomy');
-      }
-      if (params[0] == 'autonomy') {
-        console.log(tag, 'Start following me in AUTONOMY mode.');
-        exec('docker start ofm && docker exec -d ofm python3 main.py --ohmni autonomy');
-      }
+      if (params.length != 1)
+        return this.log(tag, 'start_following_me <following me mode>');
+      this.cmd_camera_start([]);
+      botnode.autonomyController.cmd_startFollowingMe(params[0]);
     }
+    // Assume that camera is opened before we start following me
+    BotShell.prototype.cmd_start_following_me_ll = function (params) {
+      if (params.length != 1)
+        return this.log(tag, 'start_following_me_ll <following me mode>');
+      botnode.autonomyController.cmd_startFollowingMe(params[0]);
+    }
+    // Stop
     BotShell.prototype.cmd_stop_following_me = function (params) {
-      console.log(tag, 'Stop following me.');
-      exec('docker stop ofm');
+      botnode.autonomyController.cmd_stopFollowingMe();
     }
+
   }
 
+  notify(botnode, funcName, status, mode = '') {
+    return botnode._api.sendJson({
+      type: 'autonomy_bot_response',
+      msg: {
+        type: funcName,
+        msg: {
+          status: status,
+          message: mode,
+        }
+      }
+    });
+  }
 }
 
 module.exports = FollowingMePlugin;
